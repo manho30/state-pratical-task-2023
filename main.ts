53dbcaf765a47b88772e5c85cd3438ffaa1c3bdb
manho <86037446+manho30@users.noreply.github.com>
2024-06-05 14:28:26 +0800

    chore: merge pull req

--- main.ts
+++ main.ts
@@ -1,27 +1,28 @@
-/**
- * @author manho <manho30@outlook.my>
- */
 // Line tracker for reromicro
 function lineTracker () {
     reromicro.ReadLineSensors()
-    if (reromicro.LineSensorDetectsLine(LineSensors.Left) && (reromicro.LineSensorDetectsLine(LineSensors.Center) && reromicro.LineSensorDetectsLine(LineSensors.Right))) {
+    if (reromicro.LineSensorDetectsLine(LineSensors.Center) && reromicro.LineSensorDetectsLine(LineSensors.Left)) {
+        reromicro.RunMotor(Motors.Left, 20)
+        reromicro.RunMotor(Motors.Right, 35)
+    } else if (reromicro.LineSensorDetectsLine(LineSensors.Center) && reromicro.LineSensorDetectsLine(LineSensors.Right)) {
+        reromicro.RunMotor(Motors.Left, 35)
+        reromicro.RunMotor(Motors.Right, 20)
+    } else if (reromicro.LineSensorDetectsLine(LineSensors.Center)) {
         reromicro.MoveForward(35)
-        basic.pause(100)
     } else if (reromicro.LineSensorDetectsLine(LineSensors.Left)) {
         reromicro.RunMotor(Motors.Left, 0)
         reromicro.RunMotor(Motors.Right, 35)
     } else if (reromicro.LineSensorDetectsLine(LineSensors.Right)) {
         reromicro.RunMotor(Motors.Left, 35)
         reromicro.RunMotor(Motors.Right, 0)
-    } else if (reromicro.LineSensorDetectsLine(LineSensors.Center)) {
-        reromicro.MoveForward(35)
+    } else {
+        reromicro.Brake()
     }
 }
 // Helper function for detect the obstacle
 function __detectObstacle__ () {
     if (reromicro.ReadUltrasonic() <= 20) {
         reromicro.Brake()
-        basic.pause(100)
     }
 }
 while (true) {
